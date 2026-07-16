c549a5c8b0439614184b08d690e1bee687e47d1e
ismailnaser <ismailnaser67@gmail.com>
2026-07-16 17:21:57 +0300

    update

--- config/settings.py
+++ config/settings.py
@@ -121,7 +121,10 @@ if IS_PRODUCTION:
             "PORT": os.environ.get("DB_PORT", "3306"),
             "OPTIONS": {
                 "charset": "utf8mb4",
-                "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+                "init_command": (
+                    "SET sql_mode='STRICT_TRANS_TABLES', "
+                    "NAMES 'utf8mb4' COLLATE 'utf8mb4_unicode_ci'"
+                ),
             },
         }
     }
